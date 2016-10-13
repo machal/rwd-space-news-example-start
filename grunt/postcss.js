5ac5bbcc0736da5f0d9baa2d1c2ca74a701238a7
Martin Michálek <michalek@shortcat.cz>
2016-10-13 12:47:13 +0200

    PostCSS - fixed

--- grunt/postcss.js
+++ grunt/postcss.js
@@ -7,21 +7,23 @@
 
 module.exports = {
 
-  options: {
-    map: true,
-    processors: [
-      // rem -> px fallback:
-      require('pixrem')({rootValue: 16}),
-      // pridani vendor prefixu
-      require('autoprefixer')({browsers: ['last 3 versions', 'ios 6', 'ie 7', 'ie 8', 'ie 9']}),
-      // media queries z px do em
-      require('postcss-em-media-query')({ }),
-      //  minifikace CSS
-      require('cssnano')()
-    ]
-  },
-  files: {
-    'dist/css/style.min.css':'dist/css/style.css'
+  default: {
+    options: {
+      map: true,
+      processors: [
+        // rem -> px fallback:
+        require('pixrem')({rootValue: 16}),
+        // pridani vendor prefixu
+        require('autoprefixer')({browsers: ['last 3 versions', 'ios 6', 'ie 7', 'ie 8', 'ie 9']}),
+        // media queries z px do em
+        require('postcss-em-media-query')({ }),
+        //  minifikace CSS
+        require('cssnano')()
+      ]
+    },
+    files: {
+      'dist/css/style.min.css':'dist/css/style.css'
+    }
   }
 
 };
